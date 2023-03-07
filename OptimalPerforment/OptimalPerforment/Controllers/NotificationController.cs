62fe807f36de6916d9380366831c4e1e593d24c7
hieubd <buiduchieuuet16@gmail.com>
2023-03-07 17:31:58 +0700

    up

--- OptimalPerforment/OptimalPerforment/Controllers/NotificationController.cs
+++ OptimalPerforment/OptimalPerforment/Controllers/NotificationController.cs
@@ -1,5 +1,6 @@
 ﻿using Dapper;
 using Microsoft.AspNetCore.Mvc;
+using System.Data;
 using System.Data.SqlClient;
 
 namespace OptimalPerforment.Controllers
@@ -10,20 +11,25 @@ namespace OptimalPerforment.Controllers
     {
         private readonly string ConnectionString = "data source=222.252.27.138,23424\\sql2019;initial catalog=Trainer;persist security info=True;user id=sa;password=247@dev2021;multipleactiveresultsets=True;Max Pool Size=1000;Connect Timeout=600;";
         [HttpGet]
-        public async Task Get()
+        public async Task<int> Get()
         {
             string sql = "SELECT * FROM Notification";
 
             // Create a new SqlConnection object
-            using (SqlConnection connection = new SqlConnection(ConnectionString))
-            {
-                // Open the connection
-                await connection.OpenAsync();
+            //using (SqlConnection connection = new SqlConnection(ConnectionString))
+            //{
+            //    // Open the connection
+            //    await connection.OpenAsync();
 
-                // Execute the query and return the results
-                var data = await connection.QueryAsync<Notification>(sql);
+            //    // Execute the query and return the results
+            //    var data = await connection.QueryAsync<Notification>(sql, buffered: false);
 
-            }
+            //}
+            using IDbConnection connection = new SqlConnection(ConnectionString);
+
+            var result = await connection.QueryAsync<Notification>(sql);
+
+            return 1;
         }
     }
 
